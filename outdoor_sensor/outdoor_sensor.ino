2aca2066bd8877280ad755461dc503a1cdbc93ff
addamoom <adam.stenseke@gmail.com>
2020-12-22 14:26:37 +0100

    arduino code for demonstration

--- outdoor_sensor/outdoor_sensor.ino
+++ outdoor_sensor/outdoor_sensor.ino
@@ -4,27 +4,33 @@
 #define outpin 4
 #define length_of_bit  1000 //us, max 16000 according to delayMicro docs
 #define carrier_period 100 //us
+#define periods 10 // how many periods a carrier wave is, = lenght_of_bit/carrier_period
 
 LM75 sensor; //initialize sensor with A0->GND, A1->GND and A2->GND
-//kommer kommunicera på a4 och a5 tror jag
+	     //communicates via pin a4 and a5
 
-  void setup()
+void setup()
 {
   Wire.begin();
   Serial.begin(9600);
   pinMode(outpin, OUTPUT);
 }
 
-//currently breaks if temp is negative or over 100 but whatever
+//currently breaks if temp is over 100, but thats not very useful anyway
 void loop()
 {
   float tempraw = sensor.temp();
-  int value = (tempraw * 100.0) + 0.5;
+  int value = (tempraw * 100.0)+ 0.5; //0.5 to round up
   char thousandsDigit = '0' + ((value / 1000) % 10);
   char hundredsDigit = '0' + ((value / 100) % 10);
   char tensDigit =  '0' + ((value / 10) % 10);
   char onesDigit =  '0' + (value % 10);
+  String sign = "0";
 
+  if(tempraw < 0)
+	sign = "1";
+  
+/* 
   Serial.print("\nTemp: ");
   Serial.print(to4bit(thousandsDigit));
   Serial.print(" ");
@@ -34,42 +40,45 @@ void loop()
   Serial.print(" ");
   Serial.print(to4bit(onesDigit));
   Serial.print(" C\n");
- 
-  String msg = "110100000" + to4bit(thousandsDigit)  + to4bit(hundredsDigit) +to4bit(tensDigit) + to4bit(onesDigit);
-  Serial.print("message: ");
-  Serial.print(msg);
+*/
+
+  String msg = "1101" + sign + "0000" + to4bit(thousandsDigit)  + to4bit(hundredsDigit) +to4bit(tensDigit) + to4bit(onesDigit);
+  
+  //Serial.print("message: ");
+  //Serial.print(msg);
+  
   transmit(msg);  
-  delay(2500);
+  delay(500);
+
 }
 
+//Trasnmits either 0, which is equal to nothing, or 1, being a carrier wave
 void transmit(String data) {
   
-  send_carrier_wave(length_of_bit, carrier_period);  //start signal
+  send_carrier_wave();  //start signal
   
   for (int i = 0; i < data.length(); i++) {
     if (data[i]=='0') {
-      //Serial.print(data[i]);
       delayMicroseconds(length_of_bit-5);
     }
     else {
-      //Serial.print(data[i]);
-      send_carrier_wave(length_of_bit,carrier_period);
+      send_carrier_wave();
     }
   }
 }
 
-void send_carrier_wave(long duration, long wave_period){
-  //long periods = duration/wave_period;
-  int periods = 10; 
-  //Serial.print(periods);
+//generate a carrier wave
+void send_carrier_wave(){
+
   for(int i = 0; i < periods; i++) {
     digitalWrite(outpin, HIGH);
     delayMicroseconds(48);
     digitalWrite(outpin, LOW);
     delayMicroseconds(48);
   }
-}
 
+}
+//conversion table from char to string of bits
 String to4bit(char d){
  if(d=='0')
     return "0000";
